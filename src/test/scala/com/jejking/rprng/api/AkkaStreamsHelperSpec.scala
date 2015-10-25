221127c1c86f26be9f758b9bfbd087a59ce33e5c
John King <jejking@Johns-MBP.fritz.box>
2015-10-25 16:54:26 +0100

    routing for /byte/block set up and tested

---
+++ src/test/scala/com/jejking/rprng/api/AkkaStreamsHelperSpec.scala
@@ -0,0 +1,8 @@
+package com.jejking.rprng.api
+
+/**
+ * Created by jejking on 24/10/15.
+ */
+class AkkaStreamsHelperSpec {
+
+}
---
+++ src/test/scala/com/jejking/rprng/rng/TestUtils.scala
@@ -0,0 +1,8 @@
+package com.jejking.rprng.rng
+
+/**
+ * Created by jejking on 24/10/15.
+ */
+object TestUtils {
+
+}
