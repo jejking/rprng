a3cd42b9457e1ed00b15462d0aea867b1d8c75ad
John King <jejking@yahoo.de>
2015-11-11 15:26:01 +0000

    add documentation on protocol

--- src/main/scala/com/jejking/rprng/api/RandomIntegerCollectionResponseProtocol.scala
+++ src/main/scala/com/jejking/rprng/api/RandomIntegerCollectionResponseProtocol.scala
@@ -4,7 +4,7 @@ import com.jejking.rprng.rng.RandomIntegerCollectionResponse
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 /**
- * Created by jking on 11/11/15.
+ * Spray Json protocol object to allow response to be marshalled to JSON in the API.
  */
 object RandomIntegerCollectionResponseProtocol extends DefaultJsonProtocol {
    implicit val format: RootJsonFormat[RandomIntegerCollectionResponse] = jsonFormat1(RandomIntegerCollectionResponse.apply)
