fb24909fa9e6e87ec942630198dea5f38b68115b
John King <jejking@yahoo.de>
2015-12-11 10:31:10 +0100

    add rudimentary config, removed dead letter logging

--- src/main/scala/com/jejking/rprng/api/Main.scala
+++ src/main/scala/com/jejking/rprng/api/Main.scala
@@ -7,6 +7,7 @@ import akka.http.scaladsl.Http
 import akka.routing.RandomGroup
 import akka.stream.ActorMaterializer
 import com.jejking.rprng.rng.{RandomGeneratorFactory, RandomGeneratorSource, RandomSourceActor, SecureRandomSeeder}
+import com.typesafe.config.ConfigFactory
 import org.apache.commons.math3.random.ISAACRandom
 
 /**
@@ -18,20 +19,26 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
+    val conf = ConfigFactory.load()
+    val port = conf.getInt("rprng.port")
+    require(port > 0, s"Port must be greater than zero, but is $port")
+    val actorCount = conf.getInt("rprng.actorCount")
+    require(actorCount > 0, s"Actor count must be greater than zero, but is $actorCount")
+
     implicit val actorSystem = ActorSystem("rprng")
     implicit val materializer = ActorMaterializer()
 
-    val randomRouter = createActors(actorSystem)
+    val randomRouter = createActors(actorSystem, actorCount)
     val streamsHelper = new AkkaStreamsHelper()
     val route = new Routes(streamsHelper).route
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(route, "localhost", port)
 
     // TODO - log shutdown
   }
 
-  private def createActors(actorSystem: ActorSystem): ActorRef = {
-    for (i <- 1 to 8) {
+  private def createActors(actorSystem: ActorSystem, actorCount: Int): ActorRef = {
+    for (i <- 1 to actorCount) {
       val secureRandom = new SecureRandom()
       val secureSeeder = new SecureRandomSeeder(secureRandom)
       val randomGenerator = RandomGeneratorFactory.createNewGeneratorInstance[ISAACRandom]
