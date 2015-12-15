132c73bec2f47782611d2d68248ae7a052775a50
John King <jejking@yahoo.de>
2015-12-15 12:50:14 +0100

    more extensive load test

--- gatling/src/test/scala/com/jejking/rprng/gatling/RprngLoadSimulation.scala
+++ gatling/src/test/scala/com/jejking/rprng/gatling/RprngLoadSimulation.scala
@@ -3,6 +3,7 @@ package com.jejking.rprng.gatling
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
+import scala.util.Random
 
 /**
  * Load and stress test for a locally running RPRNG web service. The intention
@@ -11,22 +12,47 @@ import scala.concurrent.duration._
  */
 class RprngLoadSimulation extends Simulation {
 
-  // most basic scenario, just grab 1kb of random bytes repeatedly, in parallel
   val httpConf = http.baseURL("http://localhost:8080")
 
-  val scn = scenario("bytes")
-    .exec(http("bytes").get("/byte/block").check(status.is(200)))
+  val byteBlockSizeFeeder = Iterator.continually(Map("blockSize" -> (Random.nextInt(768) + 256)))
 
-  setUp(scn.inject(rampUsersPerSec(10) to 250 during(10 minutes) randomized)
-    .protocols(httpConf)).maxDuration(15 minutes)
+  val bytesScn = scenario("bytes")
+                  .feed(byteBlockSizeFeeder)
+                  .exec(http("byte block")
+                    .get("/byte/block/${blockSize}")
+                    .check(status.is(200)))
 
 
-  // lets go for:
-  // - some randomly sized lists
-  // - some randomly sized sets
-  // - some blocks of bytes
 
-  // do this over 30 minutes
+  // size, count, min, max
+  val collectionFeeder = Iterator.continually(Map("val" -> Map("size" -> (Random.nextInt(10) + 1),
+                                                              "count" -> (Random.nextInt(10) + 1),
+                                                              "min" -> Random.nextInt(10),
+                                                              "max" -> (Random.nextInt(100) + 100))))
+
+
+  val intListScn = scenario("int list")
+                    .feed(collectionFeeder)
+                    .exec(http("int list")
+                      .get("/int/list")
+                      .queryParamMap("${val}")
+                      .check(status.is(200)))
+
+  val intSetScn = scenario("int set")
+                  .feed(collectionFeeder)
+                  .exec(http("int set")
+                    .get("/int/set")
+                    .queryParamMap("${val}")
+                    .check(status.is(200)))
+
+  val scnList = List(bytesScn, intListScn, intSetScn)
+
+
+  setUp(scnList.map(scn =>
+                    scn.inject(constantUsersPerSec(100) during (30 minutes))
+                    .protocols(httpConf)))
+    .throttle(reachRps(300) in (10 minutes), holdFor(20 minutes))
+    .maxDuration(30 minutes)
 
 
 }
