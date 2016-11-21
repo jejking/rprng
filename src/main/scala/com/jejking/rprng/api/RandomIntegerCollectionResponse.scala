a643cfced967961c07fffae4d0b49ce477fa56f1
jking <jejking@thoughtworks.com>
2016-11-21 14:44:32 +0100

    some tweaks

--- src/main/scala/com/jejking/rprng/api/RandomIntegerCollectionResponse.scala
+++ src/main/scala/com/jejking/rprng/api/RandomIntegerCollectionResponse.scala
@@ -1,8 +1,17 @@
 package com.jejking.rprng.api
 
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+
 /**
  * Encapsulates response to a request for some random integers.
  */
 case class RandomIntegerCollectionResponse(content: Iterable[Iterable[Int]])
+
+/**
+  * Spray Json protocol object to allow response to be marshalled to JSON in the API.
+  */
+object RandomIntegerCollectionResponseProtocol extends DefaultJsonProtocol {
+  implicit val format: RootJsonFormat[RandomIntegerCollectionResponse] = jsonFormat1(RandomIntegerCollectionResponse.apply)
+}
 
 
