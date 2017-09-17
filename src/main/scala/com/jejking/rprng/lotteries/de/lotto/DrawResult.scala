cf8a73d4cedcd93e39f35fcebc7a55e170d049ab
John King <jejking@thoughtworks.com>
2017-09-17 20:30:48 +0200

    changes DrawResult from using Integer to native scala Int

--- src/main/scala/com/jejking/rprng/lotteries/de/lotto/DrawResult.scala
+++ src/main/scala/com/jejking/rprng/lotteries/de/lotto/DrawResult.scala
@@ -9,9 +9,9 @@ package com.jejking.rprng.lotteries.de.lotto
   * @param superNumber an integer between 0 and 9 inclusive
   * @throws IllegalArgumentException if input parameters do not meet the above requirements
   */
-case class DrawResult(numbers: Set[Integer], superNumber: Integer) {
+case class DrawResult(numbers: Set[Int], superNumber: Int) {
 
   require(numbers.size == 6, s"Numbers set must be of size 6. It was of size ${numbers.size}.")
-  numbers.foreach((i:Integer) => require(i >= 1 && i <= 49, "Numbers set may only contain numbers between 1 and 49 inclusive."))
+  numbers.foreach((i:Int) => require(i >= 1 && i <= 49, "Numbers set may only contain numbers between 1 and 49 inclusive."))
   require(superNumber >= 0 && superNumber <= 9, "The superNumber may only be between 0 and 9 inclusive.")
 }
