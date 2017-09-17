8aed71cf7cb24a2981a4dd159f7ac9e3583d4c46
John King <jejking@thoughtworks.com>
2017-09-17 20:28:53 +0200

    adds error message to EightByteString validation

--- src/main/scala/com/jejking/rprng/rng/EightByteString.scala
+++ src/main/scala/com/jejking/rprng/rng/EightByteString.scala
@@ -6,5 +6,5 @@ import akka.util.ByteString
   * Wraps a byte string that is ensured to be of length 8.
   */
 case class EightByteString(byteString: ByteString) {
-  require(byteString.length == 8)
+  require(byteString.length == 8, s"ByteString is not of length 8. It is of length ${byteString.length}")
 }
